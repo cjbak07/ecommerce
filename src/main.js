b5ac7d85f9c8e33385aaed69e2ddc2ec4c08c5c1
Cristhian Heber <cristhianheber13@gmail.com>
2025-06-02 16:43:59 -0300

    Mensagem descrevendo a atualização

--- src/main.js
+++ src/main.js
@@ -2,10 +2,6 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import './index.css'; // Tailwind CSS import (gerado via Vite)
-
 const app = createApp(App);
-
 app.use(router);
-
 app.mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,5 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
 import CartView from '../views/CartView.vue';
 import HomeView from '../views/HomeView.vue';
 import ProductDetails from '../views/ProductDetails.vue';
@@ -7,3 +9,10 @@ const routes = [
   { path: '/product/:id', name: 'ProductDetails', component: ProductDetails },
   { path: '/cart', name: 'Cart', component: CartView },
 ];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
